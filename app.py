2166c8e9c5fb6f85d22729993dbf3a727cb0dc20
sgrscsurya <sgrscsurya@gmail.com>
2025-11-22 17:57:36 +0530

    Changed the Port to make the Web Service Live on Render

--- app.py
+++ app.py
@@ -104,4 +104,6 @@ def generate():
     return render_template("index.html", result=result, situation=situation, num_excuses=num_excuses)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
